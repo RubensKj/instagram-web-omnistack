b02cca8989eb67bae7d482d4d8217470d91023df
RubensKj <36218162+RubensKj@users.noreply.github.com>
2020-06-10 23:29:50 -0300

    Fixing bugs

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -42,35 +42,35 @@ class Feed extends Component {
     render() {
         return (
             <section id="post-list">
-                { this.state.feed.map(post => (
+                {this.state.feed && this.state.feed.map(post => (
                     <article key={post.id}>
-                    <header>
-                        <div className="user-info">
-                            <span>{post.author}</span>
-                            <span className="place">{post.place}</span>
-                        </div>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
 
-                        <img src={more} alt="Mais" />
-                    </header>
-                    <img src={`${apiUrl}/uploads/resized/${post.image}`} alt="" />
+                            <img src={more} alt="Mais" />
+                        </header>
+                        <img src={`${apiUrl}/uploads/resized/${post.image}`} alt="" />
 
-                    <footer>
-                        <div className="actions">
-                            <button type="button" onClick={() => this.handleLike(post.id)}>
-                                <img src={like} alt="" />
-                            </button>
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <button type="button" onClick={() => this.handleLike(post.id)}>
+                                    <img src={like} alt="" />
+                                </button>
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
 
-                        <strong>{post.like} curtidas</strong>
+                            <strong>{post.like} curtidas</strong>
 
-                        <p>
-                            {post.description}
-                            <span>{post.hashtags}</span>
-                        </p>
-                    </footer>
-                </article>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
                 ))}
             </section>
         );
